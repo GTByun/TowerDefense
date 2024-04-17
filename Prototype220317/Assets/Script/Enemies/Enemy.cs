e843a4ff70dbe65aad81996d71bbe01f5db9232d
KittoAndFriendo <ruddn9040@gmail.com>
2024-04-18 07:35:04 +0900

    잡다 2개
    - Enum를 따로 쉽게 관리할 수 있도록 EnumScript를 만들었습니다. Enum이 스크립트 곳곳에 있는 것 보다는 한 곳에 다같이 있으면 보기 편할 듯??? 잘 몰루
    - StateManager 뼈대 만들다가 밥먹으러 감

--- Prototype220317/Assets/Script/Enemies/Enemy.cs
+++ Prototype220317/Assets/Script/Enemies/Enemy.cs
@@ -14,13 +14,7 @@ public class Enemy : MonoBehaviour
     private float synthSpeed;
     private float modular;
 
-    enum EnemyMove
-    {
-        Up = 0,
-        Right = 1,
-        Down = 2,
-        Left = 3
-    }
+
     private EnemyMove move;
 
     public float Speed { set => speed = value; }
---
+++ Prototype220317/Assets/Script/Managers/EnumScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnumScript
+{
+}
+
+//여기에 enum들 넣어주세요
+
+/// <summary>
+/// 적 오브젝트가 어느 방향으로 이동해야 하는지를 저장함
+/// </summary>
+enum EnemyMove
+{
+    Up = 0,
+    Right = 1,
+    Down = 2,
+    Left = 3
+}
+
+/// <summary>
+/// 현재 게임의 State
+/// </summary>
+enum GameState
+{
+
+}
