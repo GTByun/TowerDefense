fdd451cb2e29086ae162dbf3a11dabbeda753636
KittoAndFriendo <ruddn9040@gmail.com>
2024-05-22 09:32:33 +0900

    카드 중복 제거

--- Prototype220317/Assets/Script/Managers/CardManager.cs
+++ Prototype220317/Assets/Script/Managers/CardManager.cs
@@ -26,9 +26,12 @@ public class CardManager : MonoBehaviour
             if (newCard == excludeCard) newCard++;
             if (gameManager.gridManager.HasTower(newCard) != -1 && Random.Range(0f, 1f) < moduleChance) //만약 이미 이 타워를 가지고 있고, 확률이 된다면 모듈 타워를 대신 카드에 저장함
             {
-                newCard += nTower / 2;
+                card.GetComponent<CardScript>().SetCard(newCard + nTower / 2);
+            }else
+            {
+                card.GetComponent<CardScript>().SetCard(newCard);
             }
-            card.GetComponent<CardScript>().SetCard(newCard);
+            
             newCard++;
         }
     }
