659a2cc911a2ff680f1b24b2ae00821a8c5bab11
GTByun <byungt02@naver.com>
2024-06-14 11:25:29 +0900

    QuickQuit Add

--- Prototype220317/Assets/Script/Title/ButtonManager.cs
+++ Prototype220317/Assets/Script/Title/ButtonManager.cs
@@ -6,21 +6,6 @@ using UnityEngine.UI;
 
 public class ButtonManager : MonoBehaviour
 {
-    [SerializeField] private GameObject credit;
-    [SerializeField] private GameObject help;
-    [SerializeField] private bool creditComplete = false;
-    [SerializeField] private bool helpComplete = false;
-
-    private void Start()
-    {
-        credit.SetActive(creditComplete);
-        help.SetActive(helpComplete);
-#if UNITY_EDITOR
-        credit.SetActive(true);
-        help.SetActive(true);
-#endif
-    }
-
     public void GameStart() => SceneManager.LoadScene("GameScene");
 
     public void GameQuit() => Application.Quit();
